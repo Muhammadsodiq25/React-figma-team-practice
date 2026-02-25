8c4818b7e3d1075f57ed11d7de3a444751358fe3
rahmatullo995 <rahmatulo@gmail.com>
2026-02-25 08:56:19 +0500

    Rahmatullo 01

---
+++ src/data/Rahmatullo.js
@@ -0,0 +1,101 @@
+export const cardLink = [
+    {
+        cardOne: [
+            {
+                name: 'PN0001245',
+                heroName: 'Medical App (iOS native)',
+                titleE: 'View details',
+            },
+            {
+                nameE: 'PN0001245',
+                title: 'Food Delivery Service',
+            }
+        ],
+        cardTasks: [
+            {
+                Card1: [
+                    {
+                        taskName: 'Task Name',
+                        NameTsk: 'Research',
+                        Estimate: 'Estimate',
+                        NameEst: '2d 4h',
+                        SpentTime: 'Spent Time',
+                        NameSpnt: '1d 2h',
+                        ForAssign: ImageMedium,
+                        Priroty: 'Priroty',
+                        ForPriroty: 'Medium'
+                    }
+                ],
+                Card2: [
+                    {
+                        taskName: 'Task Name',
+                        NameTsk: 'Mind Map',
+                        Estimate: 'Estimate',
+                        NameEst: '1d 2h',
+                        SpentTime: 'Spent Time',
+                        NameSpnt: '4h 25m',
+                        ForAssign: ImageMedium,
+                        Priroty: 'Priroty',
+                        ForPriroty: 'Medium'
+                    }
+                ],
+                Card3: [
+                    {
+                        taskName: 'Task Name',
+                        NameTsk: 'UX sketches',
+                        Estimate: 'Estimate',
+                        NameEst: '1d 2h',
+                        SpentTime: 'Spent Time',
+                        NameSpnt: '4h 25m',
+                        Assign: 'Assign',
+                        ForAssign: ImageLowOne,
+                        Priroty: 'Priroty',
+                        ForPriroty: 'Low'
+                    }
+                ],
+                Card4: [
+                    {
+                        taskName: 'Task Name',
+                        NameTsk: 'UX Login + Registration',
+                        Estimate: 'Estimate',
+                        NameEst: '1d 2h',
+                        SpentTime: 'Spent Time',
+                        NameSpnt: '4h 25m',
+                        Assign: 'Assign',
+                        ForAssign: ImageLowTwo,
+                        Priroty: 'Priroty',
+                        ForPriroty: 'Low'
+                    }
+                ],
+                Card5: [
+                    {
+                        taskName: 'Task Name',
+                        NameTsk: 'UI Login + Registration',
+                        Estimate: 'Estimate',
+                        NameEst: '1d 2h',
+                        SpentTime: 'Spent Time',
+                        NameSpnt: '4h 25m',
+                        Assign: 'Assign',
+                        ForAssign: ImageMedium,
+                        Priroty: 'Medium',
+                        ForPriroty: 'Low'
+                    }
+                ],
+                Card6: [
+                    {
+                        taskName: 'Task Name',
+                        NameTsk: 'UX sketches',
+                        Estimate: 'Estimate',
+                        NameEst: '1d 2h',
+                        SpentTime: 'Spent Time',
+                        NameSpnt: '4h 25m',
+                        Assign: 'Assign',
+                        ForAssign: ImageLowOne,
+                        Priroty: 'Priroty',
+                        ForPriroty: 'Low'
+                    }
+                ],
+            }
+        ]
+    }
+]
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,8 +1,91 @@
+import './Projects.css'
+import ImageMedium from '../../assets/MediumOne.png'
+import ImageLowOne from '../../assets/LowOne.png'
+import ImageLowTwo from '../../assets/LowTwo.png'
+import { Link } from 'react-router-dom'
 
 const Projects = () => {
   return (
-    <div>
-      
+    <div className='ConTainer'>
+      <h3>
+        <p><Link>Info</Link></p>
+        <p><Link>About</Link></p>
+        <p><Link>Use</Link></p>
+      </h3>
+        <div className="Container">
+      <div className="LeftDiv">
+        <h3>Current Projects</h3>
+                <main className="TextMain">
+                    <p>PN0001245</p>
+                    <h3>Medical App (iOS native)</h3>
+                    <p>View details</p>
+                </main>
+                <main className="TextMain">
+                    <p>PN0001245</p>
+                    <h3>Medical App (iOS native)</h3>
+                    <p>View details</p>
+                </main>
+                <main className="TextMain">
+                    <p>PN0001245</p>
+                    <h3>Medical App (iOS native)</h3>
+                    <p>View details</p>
+                </main>
+                <main className="TextMain">
+                    <p>PN0001245</p>
+                    <h3>Medical App (iOS native)</h3>
+                    <p>View details</p>
+                </main>
+                <main className="TextMain">
+                    <p>PN0001245</p>
+                    <h3>Medical App (iOS native)</h3>
+                    <p>View details</p>
+                </main>
+                <main className="TextMain">
+                    <p>PN0001245</p>
+                    <h3>Medical App (iOS native)</h3>
+                    <p>View details</p>
+                </main>
+      </div>
+      <div className='Task'>
+          <p className='TaskNav'>Activ Tasks</p>
+              <div className='Div'>
+                  <p>Task Name <span>Research</span></p>
+                  <p>Estimate<span>2d 4h</span></p>
+                  <p>Spent Time<span>2d 4h</span></p>
+                  <p>Assignee <img src={ImageMedium}/></p>
+              </div>
+              <div className='Div'>
+                  <p>Task Name <span>Research</span></p>
+                  <p>Estimate<span>2d 4h</span></p>
+                  <p>Spent Time<span>2d 4h</span></p>
+                  <p>Assignee <img src={ImageMedium}/></p>
+              </div>
+              <div className='Div'>
+                  <p>Task Name <span>Research</span></p>
+                  <p>Estimate<span>2d 4h</span></p>
+                  <p>Spent Time<span>2d 4h</span></p>
+                  <p>Assignee <img src={ImageLowOne}/></p>
+              </div>
+              <div className='Div'>
+                  <p>Task Name <span>Research</span></p>
+                  <p>Estimate<span>2d 4h</span></p>
+                  <p>Spent Time<span>2d 4h</span></p>
+                  <p>Assignee <img src={ImageLowTwo}/></p>
+              </div>
+              <div className='Div'>
+                  <p>Task Name <span>Research</span></p>
+                  <p>Estimate<span>2d 4h</span></p>
+                  <p>Spent Time<span>2d 4h</span></p>
+                  <p>Assignee <img src={ImageMedium}/></p>
+              </div>
+              <div className='Div'>
+                  <p>Task Name <span>Research</span></p>
+                  <p>Estimate<span>2d 4h</span></p>
+                  <p>Spent Time<span>2d 4h</span></p>
+                  <p>Assignee <img src={ImageMedium}/></p>
+              </div>
+      </div>
+    </div>
     </div>
   )
 }
